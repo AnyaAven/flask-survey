a338120aff9c9ab1a21250415a1839fdc9f962b1
Anya Aven <leafeon96@gmail.com>
2024-04-25 18:53:51 -0400

    Added route for anwer and added global var

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
+curr_question_id = 0
 
 @app.get("/")
 def display_home():
@@ -21,14 +22,14 @@ def display_home():
         instructions=survey.instructions
         )
 
-# Posts can never be cached %100 your browser will send a request, with get
+# Posts can never be cached 100% your browser will send a request.
 # with get methods your browsers will decide if it needs to make a request.
 # As a dev I have no control over the get
 
 @app.post("/begin")
 def redirect_questions():
     """ Redirect to questions """
-    q_id = 0
+    q_id = str(curr_question_id)
 
     return redirect(f"/questions/{q_id}")
 
@@ -38,8 +39,33 @@ def display_question(q_id):
     """ Display question"""
 
     q_id = int(q_id)
+    global curr_question_id
+    curr_question_id += 1
 
     return render_template(
         "question.jinja",
-        question=survey.questions[q_id]
+        question=survey.questions[q_id],
+        id=q_id
     )
+
+@app.post("/answer")
+def handle_answer():
+    """ Redirect to next question or show completion if no other questions"""
+
+    global curr_question_id
+
+    if curr_question_id >= len(survey.questions):
+        curr_question_id = 0
+
+        return render_template(
+            "completion.jinja",
+            answers=responses
+        )
+
+    answer = request.form.get("answer")
+    responses.append(answer)
+    print("RESPONSES", answer, responses)
+
+    q_id = str(curr_question_id)
+
+    return redirect(f"/questions/{q_id}")
